a5b2da2ff311ce2c92b571cfa8056ecb9d838ee6
poliestireno <poliestireno@gmail.com>
2025-10-08 14:59:32 +0200

    final calculadora

--- CALC/calc.php
+++ CALC/calc.php
@@ -1,26 +1,36 @@
 <?php
 //var_export($_POST);
 $res = 0;
-switch ($_POST['op']) {
-    case '+':
-        //echo "suma";
-        $res = $_POST['n1'] + $_POST['n2'];
-        break;
-    case '-':
-        //echo "resta";
-        $res = $_POST['n1'] - $_POST['n2'];
-        break;
-    case 'x':
-        //echo "mul";
-        $res = $_POST['n1'] * $_POST['n2'];
-        break;
-    case '/':
-        //echo "div";
-        $res = $_POST['n1'] / $_POST['n2'];
-        break;    
-    default:
-        echo "error de operación, elige una operación";
-        break;
+if (isset($_POST['op'])) // si no es la primera vez
+{
+    if (($_POST['n1']>100 )||($_POST['n2']>100))
+    {
+        echo '<script>alert("ERROR los números no pueden ser mayores de 100")</script>';
+    }
+    else
+    {
+        switch ($_POST['op']) {
+            case '+':
+                //echo "suma";
+                $res = $_POST['n1'] + $_POST['n2'];
+                break;
+            case '-':
+                //echo "resta";
+                $res = $_POST['n1'] - $_POST['n2'];
+                break;
+            case 'x':
+                //echo "mul";
+                $res = $_POST['n1'] * $_POST['n2'];
+                break;
+            case '/':
+                //echo "div";
+                $res = $_POST['n1'] / $_POST['n2'];
+                break;    
+            default:
+                echo "error de operación, elige una operación";
+                break;
+            }
+    }
 }
 ?>
 <!DOCTYPE html>
@@ -28,13 +38,12 @@ switch ($_POST['op']) {
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">   
- 
     <title>Calculadora</title>
 </head>
 <body>
     <form action="calc.php" method="post">
-        n1:<input type="text" name="n1" id="n1">   
-        n2:<input type="text" name="n2" id="n2">
+        n1:<input required="required" type="number" name="n1" id="n1">   
+        n2:<input required="required" type="number" name="n2" id="n2">
         <br/>
         <input type="submit" name="op" value="+">
         <input type="submit" name="op" value="-">
