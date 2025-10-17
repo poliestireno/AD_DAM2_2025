69dc6ee64707ea228f029530706cc5898940e372
poliestireno <poliestireno@gmail.com>
2025-10-17 18:28:46 +0200

    teminado ejercicio de adivina y comienzo de tablas

--- ADIVINA/adivina.php
+++ ADIVINA/adivina.php
@@ -4,13 +4,17 @@ $resultado="Comienza el juego";
 if (count($_POST)==0) // es la primera vez
 {
     $num_objetivo = rand(1,10);
+    $num_intentos = 0;
 }
 else // no es la primera vez
 {
+    //que vaya poniendo número de intentos
     $num_objetivo = $_POST['objetivo'];
+    $num_intentos = $_POST['intentos'] + 1;
     if ($_POST['numero']==$num_objetivo)
     {
         $resultado = "Enhorabuena de la buena";
+        echo '<script>alert("Enhorabuena lo has conseguido con '.$num_intentos.' intentos")</script>';
     }
     else if  ($_POST['numero']<$num_objetivo)
     {
@@ -20,7 +24,9 @@ else // no es la primera vez
     {
         $resultado = "El objetivo es MENOR";
     }
+
 }
+//echo "Número intentos: ".$num_intentos;
 ?>
 <!doctype html>
 <html lang="en">
@@ -32,9 +38,14 @@ else // no es la primera vez
     <title>Adivina</title>
   </head>
   <body>
+<div align="right">
+    <span style="font-size: 1.5em;" class="badge bg-warning text-dark"><?php echo $num_intentos?></span>
+</div>
+<!--script>alert("Enhorabuena de la buena lo has conseguido");</!--script-->
 <form action="adivina.php" method="post">
     <input type="hidden" name="objetivo" value="<?php echo $num_objetivo?>">
-    <div class="container">
+    <input type="hidden" name="intentos" value="<?php echo $num_intentos?>">
+    <div class="container m-5">
         <div class="alert alert-success">
             <strong><?php echo $resultado?></strong>
         </div>
---
+++ TABLAS/tabla3x3.php
@@ -0,0 +1,35 @@
+
+<!doctype html>
+<html lang="en">
+  <head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <title>Adivina</title>
+  </head>
+  <body>
+<div class="container m-5">
+
+</div>
+hacerlo con for
+<table class="table table-bordered border-primary">
+    <tr>
+      <td><?php echo rand(1,9)?></td>
+      <td><?php echo rand(1,9)?></td>
+      <td><?php echo rand(1,9)?></td>
+    </tr>
+    <tr>
+      <td><?php echo rand(1,9)?></td>
+      <td><?php echo rand(1,9)?></td>
+      <td><?php echo rand(1,9)?></td>
+    </tr>
+    <tr>
+      <td><?php echo rand(1,9)?></td>
+      <td><?php echo rand(1,9)?></td>
+      <td><?php echo rand(1,9)?></td>
+    </tr>
+</table>
+</body>
+  </body>
+</html>
